a9b82a1feaedbf2f762cbd2647ed397bd6fcbf49
wevertonneves <162976201+wevertonneves@users.noreply.github.com>
2024-03-27 19:19:09 -0300

    Add files via upload
    Declare uma string e imprima seu comprimento no console. Converta a string para letras maiúsculas. Divida a string em palavras e imprima cada palavra no console.

---
+++ Declare o Comprimento de uma string 03.js
@@ -0,0 +1,7 @@
+let string = "Declare uma string e imprima seu comprimento no console."
+let stringtamanho =string.length;
+let maiusculo = string.toUpperCase();
+let separar = string.split(" ");
+console.log("Está string tem exatamente um comprimente de:",stringtamanho);
+console.log(maiusculo);
+console.log(separar);
